532c87f97a887b3e9a566ec103dcaead43cba40c
Fabio Ramos <fabiostart@gmail.com>
2020-10-28 21:22:41 -0300

    Inclusao do Metodo Navegador

--- FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
+++ FrameworkTest/src/test/java/br/com/framework/pages/Metodos.java
@@ -1,5 +1,14 @@
 package br.com.framework.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class Metodos {
+	
+	WebDriver driver;
+	
+	public void navegador(String Url , String navegador ) {
+		
+		
+	}
 
 }
