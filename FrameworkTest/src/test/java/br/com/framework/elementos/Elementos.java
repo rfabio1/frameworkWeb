dacee13a2f3cc3811905038d78ed1fad7b8d7e87
Fabio Ramos <fabiostart@gmail.com>
2020-10-30 16:40:55 -0300

    Atualização de metodos e elementos

--- FrameworkTest/src/test/java/br/com/framework/elementos/Elementos.java
+++ FrameworkTest/src/test/java/br/com/framework/elementos/Elementos.java
@@ -3,12 +3,11 @@ package br.com.framework.elementos;
 import org.openqa.selenium.By;
 
 public class Elementos {
-	
-	private By	pesquisa = By.name("q");
+
+	private By pesquisa = By.name("q");
 
 	public By getPesquisa() {
 		return pesquisa;
 	}
-	
 
 }
